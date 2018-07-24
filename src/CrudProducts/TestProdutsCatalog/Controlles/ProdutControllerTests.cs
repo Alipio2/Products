d3e3fc8f704bc87b119a37633b70b82c20d1f934
BitBucket <alipio.ferro@hotmail.com>
2018-07-24 09:53:04 -0300

    subindo mais teste

--- src/CrudProducts/TestProdutsCatalog/Controlles/ProdutControllerTests.cs
+++ src/CrudProducts/TestProdutsCatalog/Controlles/ProdutControllerTests.cs
@@ -15,20 +15,84 @@ namespace TestProdutsCatalog.Controlles
         {
             var rp = new ProductRepository(new ProductsDataContext());
 
-            var resp =  rp.Get(id);
+            var resp = rp.Get(id);
 
-            var produtc = new Product() {
+            var produtc = new Product()
+            {
                 Id = Guid.NewGuid().ToString(),
                 Name = "Escumadeira Teste",
                 Description = "Escumaderia de alumino utilizado pelo foguete da nasa",
                 BrandName = "Nasa",
-                SellersName= "Nasa",
-                CategoryName= "Cozinha",
-                State= StateProdutc.Active,
+                SellersName = "Nasa",
+                CategoryName = "Cozinha",
+                State = StateProdutc.Active,
                 CreateDate = DateTime.Now,
                 LastUpdateDate = DateTime.Now
             };
-        Assert.AreNotEqual(produtc, resp);
+            Assert.AreNotEqual(produtc, resp);
+        }
+
+        [TestMethod]
+        public void Get(string id)
+        {
+            var rp = new ProductRepository(new ProductsDataContext());
+
+            var produtc = new Product()
+            {
+                Id = id,
+                Name = "Escumadeira Teste",
+                Description = "Escumaderia de alumino utilizado pelo foguete da nasa",
+                BrandName = "Nasa",
+                SellersName = "Nasa",
+                CategoryName = "Cozinha",
+                State = StateProdutc.Active,
+                CreateDate = DateTime.Now,
+                LastUpdateDate = DateTime.Now
+            };
+
+            rp.Create(produtc);
+            var resp = rp.Get(id);
+           
+            Assert.AreNotEqual(produtc, resp);
+        }
+
+        [TestMethod]
+        public void Put(string id)
+        {
+            var rp = new ProductRepository(new ProductsDataContext());               
+           
+            var resp = rp.Get(id);
+
+            resp.State = StateProdutc.OutOfLine;
+
+            rp.Save(resp);
+            var respalterado = rp.Get(id);
+
+            Assert.AreEqual(respalterado.State, StateProdutc.OutOfLine);
+        }
+
+        [TestMethod]
+        public void Post()
+        {
+            var rp = new ProductRepository(new ProductsDataContext());
+
+            var produtc = new Product()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Escumadeira Teste",
+                Description = "Escumaderia de alumino utilizado pelo foguete da nasa",
+                BrandName = "Nasa",
+                SellersName = "Nasa",
+                CategoryName = "Cozinha",
+                State = StateProdutc.Active,
+                CreateDate = DateTime.Now,
+                LastUpdateDate = DateTime.Now
+            };
+
+            rp.Create(produtc);
+            var resp = rp.Get(produtc.Id);
+
+            Assert.AreEqual(produtc, resp);
         }
 
     }
